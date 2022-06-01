6e3eac9c1bd23b3783072d1f185e312dc892b445
Julien <jfustier42@gmail.com>
2022-06-01 14:27:30 +0200

    first commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<link rel="stylesheet" href="/foo.css">
+<!-- <script src="/app.js"></script> -->
+
+<body>
+    {{ $slot }}
+</body>
